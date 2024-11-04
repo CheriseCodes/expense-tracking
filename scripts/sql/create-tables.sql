0ab746e802e07cb6dc226e25579264d7e22c8dd4
CheriseCodes <noreply@cherisebryan.com>
2024-11-03 20:23:46 -0500

    fix syntax

--- scripts/sql/create-tables.sql
+++ scripts/sql/create-tables.sql
@@ -2,22 +2,22 @@ CREATE TABLE users (
 	user_id uuid DEFAULT gen_random_uuid(),
 	username varchar NOT NULL,
 	email varchar NOT NULL,
-	password_hash char(73) binary, # max length using bcrypt is 72
-	role char(30) NOT NULL, # regular user, admin or something else
+	password_hash char(73) NOT NULL, -- max length using bcrypt is 72
+	role char(30) NOT NULL, -- regular user, admin or something else
 	created_at timestamp NOT NULL,
 	last_login timestamp NOT NULL,
-        categories bigint[], # array of category ids
+        categories bigint[], -- array of category ids
 	PRIMARY KEY(user_id)
 );
 
 CREATE TABLE categories (
-	category_id bigint DEFAULT nextval('integer_id_seq'),
+	category_id uuid DEFAULT gen_random_uuid(),
 	category_name varchar NOT NULL,
 	PRIMARY KEY(category_id)
 );
 
 CREATE TABLE expenses (
-	expense_id bigint DEFAULT nextval('integer_id_seq'),
+	expense_id uuid DEFAULT gen_random_uuid() UNIQUE,
 	user_id uuid NOT NULL,
 	item varchar NOT NULL,
 	vendor varchar NOT NULL,
@@ -34,8 +34,8 @@ CREATE TABLE expenses (
 );
 
 CREATE TABLE expense_categories (
-	expense_id bigint NOT NULL,
-        category_id bigint NOT NULL,
+	expense_id uuid NOT NULL,
+        category_id uuid NOT NULL,
 	PRIMARY KEY (expense_id, category_id),
 	CONSTRAINT fk_expense
 		FOREIGN KEY(expense_id)
@@ -43,22 +43,22 @@ CREATE TABLE expense_categories (
 				ON DELETE CASCADE,
         CONSTRAINT fk_category
                 FOREIGN KEY(category_id)
-                        REFERENCES expenses(category_id) 	
+                        REFERENCES categories(category_id) 	
 				ON DELETE CASCADE
 );
 
 CREATE TABLE wishlist (
-	wish_id bigint DEFAULT nextval('integer_id_seq'),
+	wish_id uuid DEFAULT gen_random_uuid(),
 	user_id uuid NOT NULL,
 	item varchar NOT NULL,
 	vendor varchar,
 	price decimal(12,2) NOT NULL,
 	priority int NOT NULL,
-	status char(20) NOT NULL, # wished, scheduled, bought
+	status char(20) NOT NULL, -- wished, scheduled, bought
 	notes varchar,
 	planned_date date,
 	created_at timestamp,
-        PRIMARY KEY (wish_id, user_id)
+        PRIMARY KEY (wish_id, user_id),
         CONSTRAINT fk_user
                 FOREIGN KEY(user_id)
                         REFERENCES users(user_id)
@@ -66,22 +66,22 @@ CREATE TABLE wishlist (
 );
 
 CREATE TABLE budgets (
-	budget_id bigint DEFAULT nextval('integer_id_seq'),
+	budget_id uuid DEFAULT gen_random_uuid(),
 	user_id uuid NOT NULL,
-	category_id bigint NOT NULL,
+	category_id uuid NOT NULL,
 	current_spend decimal(12, 2) NOT NULL,
 	future_spend decimal(12, 2) NOT NULL,
 	max_spend decimal(12, 2) NOT NULL,
 	is_over_max boolean NOT NULL,
         start_date date NOT NULL,
         end_date date NOT NULL,
-	PRIMARY KEY(budget_id, user_id)
+	PRIMARY KEY(budget_id, user_id),
 	CONSTRAINT fk_user
                 FOREIGN KEY(user_id)
                         REFERENCES users(user_id)
 				ON DELETE CASCADE,
         CONSTRAINT fk_category
                 FOREIGN KEY(category_id)
-                        REFERENCES expenses(category_id)
+                        REFERENCES categories(category_id)
 				ON DELETE CASCADE
 );
