38934d93f0e131c83453734b297bbb86d649b790
Cherise Bryan <51966982+CheriseCodes@users.noreply.github.com>
2025-10-03 22:02:44 -0400

    merge vitest config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: ['./src/test/setup.ts'],
+		css: true,
+		coverage: {
+			provider: 'v8',
+			reporter: ['text', 'json', 'html'],
+			exclude: [
+				'node_modules/',
+				'src/test/',
+				'**/*.d.ts',
+				'**/*.config.*',
+				'**/coverage/**'
+			]
+		}
+	},
   server: {
     port: 3000,
     host: true,
